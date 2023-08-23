44bde6d94287d5bf9ceac3e466adc4ec924baa0b
JustRayCB <rayancbpro@gmail.com>
2023-08-23 12:59:11 +0200

    Clear line when escape + disbling mini.bufremove

--- plugins/better-escape.lua
+++ plugins/better-escape.lua
@@ -7,7 +7,7 @@ return {
       require("better_escape").setup {
         mapping = { "jj" }, -- a table with mappings to use
         timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-        clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+        clear_empty_lines = true, -- clear line after escaping if there is only whitespace
         -- This esc is to not move the cursor to the left when back in normal mode
         keys = "<Esc>`^", -- keys used for escaping, if it is a function will use the result everytime
         -- example(recommended)
--- plugins/core.lua
+++ plugins/core.lua
@@ -25,6 +25,7 @@ return {
       return opts
     end,
   },
+  {"echasnovski/mini.bufremove", enabled = false}
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
