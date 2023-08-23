c9257e829a68ae9f4324d457cb1d8c0d6336e919
JustRayCB <rayancbpro@gmail.com>
2023-08-23 12:14:58 +0200

    Fix error when checkhealth

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "python", "cpp", "java" },
+    ensure_installed = { "lua", "python", "cpp", "java", "jsonc", "c"},
   },
 }
