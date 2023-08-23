5ae10b19e3d3dee720f44b2950be62e37ee38255
JustRayCB <rayancbpro@gmail.com>
2023-08-23 12:16:25 +0200

    Fix (I hope so) the way the cursor move "randomly" when I push tab to jump into a snippet's field

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -13,6 +13,16 @@ return {
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
     end
+    local function check_backspace()
+      local col = vim.fn.col "." - 1
+      return col == 0 or vim.fn.getline("."):sub(col, col):match "%s" ~= nil
+    end
+    luasnip.config.set_config({ -- To fix the issue of tab jumping around "randomly"
+      region_check_events = 'InsertEnter',
+      delete_check_events = 'InsertLeave',
+      -- update_events = {"TextChanged", "TextChangedI"}
+      
+    })
     return require("astronvim.utils").extend_tbl(opts, {
       window = {
         completion = {
@@ -34,55 +44,26 @@ return {
       },
       mapping = {
         ["<CR>"] = cmp.mapping.confirm { select = true },
-        -- ["<Tab>"] = cmp.mapping(function(fallback)
-        --   if luasnip.jumpable(1) then
-        --     luasnip.jump(1)
-        --   elseif cmp.visible() and has_words_before() then
-        --     cmp.confirm { select = true }
-        --   else
-        --     fallback()
-        --   end
-        -- -- end, { "i", "s" }),
+        -- Mapping both "confirm" and "jump" to <Tab> while keeping default behavior as fallback
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
+            -- if cmp.visible() then
+            --     cmp.confirm { select = true }
+            if luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            -- elseif check_backspace() then
+            --     fallback()
+            else
+                fallback()
+            end
+        end, {
+                "i",
+                "s",
+            }),
+        ["<C-p>"] = cmp.mapping(function()
+          if luasnip.jumpable(-1) then luasnip.jump(-1) end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        -- ["<Tab>"] = function(fallback)
-        --   -- if cmp.visible() then
-        --   --   cmp.select_next_item()
-        --   if luasnip.expand_or_jumpable() then
-        --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-        --   else
-        --     fallback()
-        --   end
-        -- end,
-        -- ["<S-Tab>"] = function(fallback)
-        --   -- if cmp.visible() then
-        --   --   cmp.select_prev_item()
-        --   if luasnip.jumpable(-1) then
-        --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-        --   else
-        --     fallback()
-        --   end
-        -- end,
         ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }, -- to fix the c-j not working since update 2/05/23
         ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        -- ["<C-Space>"] = cmp.mapping.complete(),
-        -- ["<C-e>"] = cmp.mapping.close(),
-        -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        -- ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-        -- ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
       },
     })
   end,
