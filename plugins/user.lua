c8fcace269ab2263c15dd2b9abf04fe7e8e8c5bc
JustRayCB <rayancbpro@gmail.com>
2023-09-29 09:23:02 +0200

    adding Vimspector mappings

--- plugins/user.lua
+++ plugins/user.lua
@@ -29,6 +29,30 @@ return {
   {
     "puremourning/vimspector",
     ft = { "python", "py", "cpp", "c", "java" },
+    init = function ()
+      vim.g.vimspector_enable_mappings = 'HUMAN'
+      vim.g.vimspector_sidebar_width = 35
+      vim.g.vimspector_code_minwidth = 70
+      vim.g.vimspector_breakpoint_symbols_enabled = 0
+      -- Disable noice before launching 
+      -- vim.api.nvim_set_keymap("n", "<leader>lla", "<cmd>call vimspector#Launch() | NoiceDisable<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>lla", ":call vimspector#Launch()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>dx", ":call vimspector#Reset()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader><leader>so", ":call vimspector#StepOut()<CR>", { noremap = true , silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>si", ":call vimspector#StepInto()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>so", ":call vimspector#StepOver()<CR>", { noremap = true , silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>d_", ":call vimspector#Restart()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>co", ":call vimspector#Continue()<CR>", { noremap = true , silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>drc", ":call vimspector#RunToCursor()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>bp", ":call vimspector#ToggleBreakpoint()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>df", ":call vimspector#ToggleConditionalBreakpoint()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>nb", ":call vimspector#JumpToNextBreakpoint()<CR>", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>de", ":VimspectorEval", { noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>dw", ":VimspectorWatch", { noremap = true , silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>>do", ":VimspectorShowOutput", { noremap = true , silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>di", "<Plug>VimspectorBalloonEval", { noremap = true , silent = true})
+      vim.api.nvim_set_keymap("x", "<leader>di", "<Plug>VimspectorBalloonEval", { noremap = true , silent = true})
+    end
   },
   {
     "kylechui/nvim-surround",
